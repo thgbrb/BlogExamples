91087fc0ed3808116dec1c42a1ca2b7b2d0e4e29
Thiago <thiago.borba@icloud.com>
2018-03-12 23:09:18 -0300

    C# 7 and Tuples examples
    C# 7 Tuples examples

---
+++ Tuples/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = OldStyle(name: "Steve", lastName: "Jobs");
+
+            WriteLine($@"A long time ago ... item1, item2, item, item -> Fullname: {p1.Item1} {p1.Item2}");
+
+            var p2 = Lookup(name: "Arthur", lastName: "Dante");
+
+            WriteLine($@"But today ... named, named :) -> Fullname: {p2.name} {p2.lastName}");
+
+            (string name, string lastName) = Lookup(name: "Bill", lastName: "Gates");
+
+            WriteLine($@"var, why? Deconstruct it! -> Fullname: {name} {lastName}");
+
+            ReadLine();
+
+        }
+
+        static Tuple<string, string> OldStyle(string name, string lastName)
+            => Tuple.Create(item1: name, item2: lastName);
+
+        static (string name, string lastName) Lookup(string name, string lastName)
+            => (name, lastName);
+    }
+}
